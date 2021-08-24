fe71a8721d452e3c49733b74f48bb424b17c12e1
monjur15 <monjur.git@gmail.com>
2021-08-24 21:34:00 +0600

    updateProductNumber Function done

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-function updateCaseNumber() {
+function updateProductNumber(product, price, isIncreasing) {
+    const caseInput = document.getElementById(product + '-number');
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
 
+    // update case total
+    const caseTotal = document.getElementById(product + '-total');
+    caseTotal.innerText = caseNumber * price;
 }
 
 
+// phone increase decrease event
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+});
+
+
+
+//  handle case increase decrease event
 document.getElementById('case-plus').addEventListener('click', function () {
 
+    updateProductNumber('case', 59, true);
 
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
 
 });
 
 document.getElementById('case-minus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) - 1;
+    updateProductNumber('case', 59, false);
+
 })
